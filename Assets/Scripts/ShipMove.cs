fe97b599dfba9acb316d23ad37d7ddf1cf8b441a
EmirRedz <48574962+EmirRedz@users.noreply.github.com>
2022-10-25 17:08:36 +0200

    Moving Ship Clouds

---
+++ Assets/Scripts/ShipMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class ShipMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMove(transform.forward * 50, 15f);
+    }
+    
+}
